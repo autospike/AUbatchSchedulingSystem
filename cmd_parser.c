2fb77746f7ee2f1e2ad5b6c707eee760e7976692
autospike <bakerjwilliam@gmail.com>
2025-02-27 12:08:23 -0600

    Checking git on second device

--- cmd_parser.c
+++ cmd_parser.c
@@ -3,5 +3,7 @@
 #include "cmd_parser.h"
 
 void cmd_parser() {
-    printf("in cmd_parser");
+    printf("Welcome to William Baker's batch job scheduler Version 1.0\n"
+        "Type 'help' to find more about aubatch commands.\n");
 }
+
